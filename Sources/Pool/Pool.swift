3c72002febcf8716be9fa7ef76abe550cc97b82e
Ryu0118 <ryunosuke.shibuya.3d@stu.hosei.ac.jp>
2023-02-25 14:45:35 +0900

    add Pool

--- Sources/Pool/Pool.swift
+++ Sources/Pool/Pool.swift
@@ -1,6 +1,22 @@
-public struct Pool {
-    public private(set) var text = "Hello, World!"
+import Foundation
+
+public struct Pool<Value, Policy>: @unchecked Sendable where Policy: CachePolicy, Value: Cacheable {
+    let cachePolicy: Policy
+
+    var isContainerNil: Bool { container == nil }
+
+    private(set) var container: Value?
+
+    private let lock = NSRecursiveLock()
 
     public init() {
+        cachePolicy = Policy()
+    }
+
+    mutating func overwriteContainer(value: Value?) {
+        defer { lock.unlock() }
+        lock.lock()
+
+        self.container = value
     }
 }

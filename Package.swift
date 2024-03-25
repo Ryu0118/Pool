80138ecddf8b58209d2a7ce9fdb868edb3922f33
KantaSwift <kanta-_-soccer.love@docomo.ne.jp>
2024-03-25 18:22:06 +0900

    include privacyInfo.xcprivacy in package resources

--- Package.swift
+++ Package.swift
@@ -24,7 +24,8 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Pool",
-            dependencies: []
+            path: "Sources",
+            resources: [.process("PrivacyInfo.xcprivacy")]
         ),
         .testTarget(
             name: "PoolTests",
